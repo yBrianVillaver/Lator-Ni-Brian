d4425b762757e32e46905ccab3dd38ad3e173f34
yBrianVillaver <claahrama01@gmail.com>
2024-07-12 21:28:34 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        // Add other resources you want to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
